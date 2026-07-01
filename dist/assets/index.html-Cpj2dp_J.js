55373cb2a54abb09d80ad41a3cfb5ed26d823341
Abdalla Sera <abdallahsira47@gmail.com>
2026-07-01 16:33:19 +0200

    Upload built dist

---
+++ dist/assets/index.html-Cpj2dp_J.js
@@ -0,0 +1,2 @@
+import{c as t,j as r,r as o}from"./index-C_LDq11T.js";import{D as e}from"./Dashboard-aBz3B-b3.js";import"./db-JT_xpHzJ.js";import"./notifications-DAqVc_9F.js";t(document.getElementById("root")).render(r.jsx(o.StrictMode,{children:r.jsx(e,{})}));
+//# sourceMappingURL=index.html-Cpj2dp_J.js.map
---
+++ dist/service-worker-loader.js
@@ -0,0 +1,1 @@
+import './assets/index.ts-CguCW3Ar.js';
